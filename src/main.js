a64acf9babf82f179c41c10fa80b220c829e979d
Vitaly Grudachev <vitaly.grudachev@servermania.com>
2021-03-18 14:22:48 +0000

    install vue-router

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
-import App from './App.vue'
 import Vuex from 'vuex'
+import VueRouter from 'vue-router'
+import App from './App.vue'
 import store from './store'
 
 Vue.config.productionTip = false
+Vue.use(VueRouter)
 Vue.use(Vuex)
 
 new Vue({
